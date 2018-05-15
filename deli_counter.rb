9a6074ac034d5240e50c7b21b4f8521ad4421290
Dennis Lord <lorddennis1006@gmail.com>
2018-05-15 19:47:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,35 +1,29 @@
-katz_deli = ["Moshe", "Fayge", "Rivki"]
-
-
-def line(array) # this was the one I figured out
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
-  else
-    puts "The line is currently empty."
-  end
-end
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
+  katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(nextinline)
-  if nextinline.empty?
-  else puts "Currently serving #{nextinline[0]}."
-    nextinline.shift
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
   end
-end
-line_method(katz_deli)
-take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Grace")
-take_a_number(katz_deli, "Kent")
-line_method(katz_deli)
-now_serving(katz_deli)
 
-take_a_number(katz_deli, "Matz")
+end
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
+  end
+
+
+end

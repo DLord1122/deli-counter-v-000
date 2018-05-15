703b711b98924e3c01654599086f4579c9728a36
Dennis Lord <lorddennis1006@gmail.com>
2018-05-15 19:44:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,7 @@ def line_method(numinline)
 end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(nextinline)

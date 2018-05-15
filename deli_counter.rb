3857c9b02881cc524718d86e37d67a999f940de2
Dennis Lord <lorddennis1006@gmail.com>
2018-05-15 19:43:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,3 +22,11 @@ def now_serving(nextinline)
     nextinline.shift
   end
 end
+line_method(katz_deli)
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
+line_method(katz_deli)
+now_serving(katz_deli)
+
+take_a_number(katz_deli, "Matz")
